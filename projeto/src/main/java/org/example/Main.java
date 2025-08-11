b59ae979ca9fab6f7a1c08515ad94963d14e85c6
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2025-08-11 08:25:08 -0300

    feat: modificando Main para testes

--- projeto/src/main/java/org/example/Main.java
+++ projeto/src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import org.example.domain.model.Cliente;
+import org.example.domain.model.Conta;
+import org.example.domain.model.ContaCorrente;
+import org.example.domain.model.ContaPoupanca;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Cliente jean = new Cliente("jean", "12345678900");
+        Conta conta1 = new Conta(jean, 123);
+
+        ContaCorrente contaCorrente1 = new ContaCorrente();
+        ContaPoupanca contaPoupanca1 = new ContaPoupanca();
+
+        Conta[] contas = {conta1, contaCorrente1, contaPoupanca1};
+
+        for (Conta conta : contas) {
+            conta.depositar(200.0);
+        }
+
+        System.out.println("------------------");
+
+        for (Conta conta : contas) {
+            conta.sacar(50.0);
+        }
+
+        contaPoupanca1.renderJuros();
+
+        System.out.println("\n--- Saldo Final das Contas ---");
+        for (Conta conta : contas) {
+            System.out.println(conta.getTipoConta() + ": R$" + conta.verSaldo());
+        }
     }
 }
